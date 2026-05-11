72f0c61969a89830ab29d33122ef257a00ea93e3
Dede <dede@Dedes-MBP.Dlink>
2026-05-11 17:18:34 -0300

    force light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" style={{ colorScheme: 'light' }}>
       <body
         className={`${chironGoRoundTC.variable} antialiased`}
       >
